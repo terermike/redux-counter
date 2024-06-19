5bf3d4b1f6b7213dc3e23610129396df7f5358f7
Terer <terermike3@gmail.com>
2024-06-19 23:18:05 +0300

    Added counter to modularize counterSplice

---
+++ src/store/counter.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterInitialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: counterInitialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export default counterSlice.reducer;
+export const counterActions = counterSlice.actions;
