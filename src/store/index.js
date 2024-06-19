4167ddbbc26b61361898ab992feecfb03c957ff9
Terer <terermike3@gmail.com>
2024-06-19 23:19:12 +0300

    Modified files to use the modulirized counter and auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,43 +1,9 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const counterInitialState = { counter: 0, showCounter: true };
-const authInitialState = { isLoggedIn: false };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: counterInitialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: "auth",
-  initialState: authInitialState,
-  reducers: {
-    login(state) {
-      state.isLoggedIn = true;
-    },
-    logout(state) {
-      state.isLoggedIn = false;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
+
 export default store;
