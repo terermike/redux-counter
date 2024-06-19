d67494134f40bdc07a1b561b36e82bafe6a9220d
Terer <terermike3@gmail.com>
2024-06-19 21:56:51 +0300

    Modified App and Auth to allow loginand logot processes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux";
 import Counter from "./components/Counter";
 import Header from "../src/components/Header";
 import Auth from "../src/components/Auth";
-
+import UserProfile from "../src/components/Auth";
 function App() {
-  // const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
     <>
       <Header />
-      <Auth />
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <UserProfile />}
       <Counter />
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,32 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store/index";
 
 const Header = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(authActions.logout());
+  }
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isLoggedIn && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
